2bf10bbd833ba9b1ec44a8887770ac1c38a781a6
Ondra <ooo.kadoun@gmail.com>
2023-11-07 10:16:40 +0100

    Update starter_master_browserstack.py

--- starter_master_browserstack.py
+++ starter_master_browserstack.py
@@ -11,7 +11,7 @@ def runner_tests_generalized(suite_general, web_brand, version, URL):
     outfile = open("results.html", "w")
     runner = HtmlTestRunner.HTMLTestRunner(log=True, verbosity=2, output='report', title=web_brand + " ||| " + URL,
                                            report_name="WEB Suite Report - version --- " + version + " ---",
-                                           open_in_browser=True, description=web_brand+" WEB Suite Report - version --- " + version + " ---")
+                                           open_in_browser=True, description=web_brand+ " WEB Suite Report - version --- " + version + " ---")
     runner.run(suite_general())
 
 
