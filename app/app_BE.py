6d385808c6c23e64dd1284860c58657d5c88399c
Ondra <ooo.kadoun@gmail.com>
2024-08-20 11:58:49 +0200

    dev app

--- app/app_BE.py
+++ app/app_BE.py
@@ -31,34 +31,32 @@ def run_suite():
     data = request.json
     logging.debug(f'Received data: {data}')
 
-    web_brand = data.get('web_brand', 'FISCHER')
+    web_brand = data.get('web_brand', 'FISCHER')  # This will now be set correctly based on the project
     version = data.get('version', 'FW-EW release 2024-05-16')
-    url = data.get('URL')  # Use URL from request
-    suite_name = data.get('suiteName')  # Get suiteName from request
+    url = data.get('URL')
+    suite_name = data.get('suiteName')
     email = data.get('email')
 
     logging.debug(f'web_brand: {web_brand}, version: {version}, URL: {url}, suiteName: {suite_name}, email: {email}')
 
     # Dictionary to map suite names to functions
     suite_mapping = {
-        'FISCHER web full suite': suite_FW_full,  # FISCHER
-        'DERRO web full suite': suite_DERRO_full,  # Derro
-        'ETRAVEL web full suite': suite_ET_full,  # eTravel
-        'EXIM web full suite': suite_EW_full,  # Exim
-        'EXIMPL web full suite': suite_EXPL_full,  # Eximpl
-        'FISCHERSK web full suite': suite_FWSK_full,  # Fischer SK
-        'KARTAGOHU web full suite': suite_KTGHU_full,  # Kartago HU
-        'KARTAGOSK web full suite': suite_KTGSK_full,  # Kartago SK
-        'NEVDAMA web full suite': suite_ND_full,  # NevDama
+        'FISCHER web full suite': suite_FW_full,
+        'DERRO web full suite': suite_DERRO_full,
+        'ETRAVEL web full suite': suite_ET_full,
+        'EXIM web full suite': suite_EW_full,
+        'EXIMPL web full suite': suite_EXPL_full,
+        'FISCHERSK web full suite': suite_FWSK_full,
+        'KARTAGOHU web full suite': suite_KTGHU_full,
+        'KARTAGOSK web full suite': suite_KTGSK_full,
+        'NEVDAMA web full suite': suite_ND_full,
     }
 
-    # Get the suite function from the mapping
     suite_function = suite_mapping.get(suite_name)
 
     if not suite_function:
         return jsonify({'status': 'error', 'message': f'Suite {suite_name} not found.'}), 400
 
-    # Run the test suite
     try:
         runner_tests_generalized(lambda: suite_function(url), web_brand, version, url, email)
         return jsonify({'status': 'success', 'message': 'Test suite executed successfully.'}), 200
